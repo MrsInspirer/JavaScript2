45f178f419db65cc281368ce3b6c8b18c2b03893
MrsInspirer <dorcashawthorn@gmail.com>
2022-06-25 21:50:22 +0000

    updating

--- javatask.js
+++ javatask.js
@@ -1,5 +1,9 @@
 alert("please enter the operation you want to perform.");
-function getInputValue()
+function getInputValue(){
+    var x=
+    document.getElementById("myText").getInputValue;
+    document.getElementById("task").innerHTML=x
+}
 alert("Example: +, -, /, * ");
 alert("number 1");
 alert(number2);
